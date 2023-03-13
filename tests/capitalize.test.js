4cfdba2bda62d0fe7c460787fd6f520b6c62bc0a
Ilya <shining1234@yandex.ru>
2023-03-13 14:38:58 +0300

    Replacing test checks with assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,9 +1,8 @@
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-}
+import { strict as assert } from 'node:assert';
+import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
+
 
 console.log('Все тесты пройдены!');
